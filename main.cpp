d0aecec60221bbd90ae4271797afc49ba80f4214
Dimitry Ivanov <dimitry@google.com>
2017-01-19 12:48:27 -0800

    Fix deadlock between vold and init
    By setting property after listeners are initialized
    we avoid deadlock between vold and init where
    vold is waiting on property_service while init is blocked
    (and therefore is not able to accept connections) on vdc
    which is attempting to communicate with vold.
    
    (This also speeds up boot by 250ms)
    
    Test: Boot a device, check locks and make sure there is no timeout
          on property_set(.)
    Test: Successfully boot a device with new property service protocol.
    Bug: http://b/34278978
    Change-Id: I9547d2f19cb35aa452bf01fbff0eb4b32a4824a4
    (cherry picked from commit c976e73bbdfd512482d0ea0c5e10afd53ba85e1e)

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@
 #include <dirent.h>
 #include <fs_mgr.h>
 
-static int process_config(VolumeManager *vm);
+static int process_config(VolumeManager *vm, bool* has_adoptable);
 static void coldboot(const char *path);
 static void parse_args(int argc, char** argv);
 
@@ -112,7 +112,9 @@ extern "C" int vold_main(int argc, char** argv) {
         exit(1);
     }
 
-    if (process_config(vm)) {
+    bool has_adoptable;
+
+    if (process_config(vm, &has_adoptable)) {
         PLOG(ERROR) << "Error reading configuration... continuing anyways";
     }
 
@@ -136,6 +138,10 @@ extern "C" int vold_main(int argc, char** argv) {
         PLOG(ERROR) << "Unable to start CryptCommandListener";
         exit(1);
     }
+
+    // This call should go after listeners are started to avoid
+    // a deadlock between vold and init (see b/34278978 for details)
+    property_set("vold.has_adoptable", has_adoptable ? "1" : "0");
 
     // Eventually we'll become the monitoring thread
     while(1) {
@@ -213,7 +219,7 @@ static void coldboot(const char *path) {
     }
 }
 
-static int process_config(VolumeManager *vm) {
+static int process_config(VolumeManager *vm, bool* has_adoptable) {
     std::string path(android::vold::DefaultFstabPath());
     fstab = fs_mgr_read_fstab(path.c_str());
     if (!fstab) {
@@ -222,7 +228,7 @@ static int process_config(VolumeManager *vm) {
     }
 
     /* Loop through entries looking for ones that vold manages */
-    bool has_adoptable = false;
+    *has_adoptable = false;
     for (int i = 0; i < fstab->num_entries; i++) {
         if (fs_mgr_is_voldmanaged(&fstab->recs[i])) {
             std::string sysPattern(fstab->recs[i].blk_device);
@@ -240,7 +246,7 @@ static int process_config(VolumeManager *vm) {
 
             if (fs_mgr_is_encryptable(&fstab->recs[i])) {
                 flags |= android::vold::Disk::Flags::kAdoptable;
-                has_adoptable = true;
+                *has_adoptable = true;
             }
             if (fs_mgr_is_noemulatedsd(&fstab->recs[i])
                     || property_get_bool("vold.debug.default_primary", false)) {
@@ -255,6 +261,5 @@ static int process_config(VolumeManager *vm) {
                                     fstype, mntopts)));
         }
     }
-    property_set("vold.has_adoptable", has_adoptable ? "1" : "0");
     return 0;
 }
